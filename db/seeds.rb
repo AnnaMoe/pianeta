c60f0cab2a111a10eca61a66913294e102e99e88
Anna Moelling <anna.moelling@googlemail.com>
2021-04-27 21:31:22 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@ puts "cleaning database..."
 Planet.destroy_all
 puts "database is clean"
 
+test_user = User.create(email: "anna@planet.de", password: "foobar")
+
 5.times do |i|
   Planet.create(
+    user: test_user,
     price: 4,
     title: "test",
     description: "test"
