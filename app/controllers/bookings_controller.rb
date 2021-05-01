888a73df9467935390f9bee3ad37cd6b52ac87c1
Anna Moelling <anna.moelling@googlemail.com>
2021-05-01 10:30:18 +0200

    implemented cloudinary, added booking route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,30 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.planet = @planet
     @booking.user = current_user
-    @booking.save
+    @booking.save!
+    redirect_to edit_planet_booking_path(@planet, @booking)
+    # else 
+    #   render ''
+    # end
+  end
+
+  def new
+    @booking =Booking.new
+    
+  end
+
+  def edit
+    @booking =Booking.find(params[:id])
+  end
+
+  def update
+    @booking =Booking.find(params[:id])
+    @booking.update(booking_params)
+      redirect_to planet_booking_path(@booking.planet, @booking)
+  end
+
+  def show
+
   end
 
   private
