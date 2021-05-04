90f66e3a7af677391bcf1b8bec8c75e3f69bdaf7
Anna Moelling <anna.moelling@googlemail.com>
2021-05-04 22:39:41 +0200

    fixed button on create page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -34,6 +34,6 @@ class BookingsController < ApplicationController
 
   private
   def booking_params
-    params.require(:booking).permit(:check_in_date, :check_out_date, :price, :location, :number_of_guests, :first_name)
+    params.require(:booking).permit(:check_in_date, :check_out_date, :price, :location, :number_of_guests, :first_namep)
   end
 end
