7eb8047b9ee1755f5765cd7658516b4e884f3565
Anna Moelling <anna.moelling@googlemail.com>
2021-05-06 17:29:56 +0200

    implemented search plugin, photo upload after create_planet on index

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -4,4 +4,11 @@ class Planet < ApplicationRecord
 
   #validates :price, :title, :description, presence: true
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
