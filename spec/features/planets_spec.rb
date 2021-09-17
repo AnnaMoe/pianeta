a3fc24d8844dbc8a42bc9b05bcfbbea3bdc2d47a
Anna Moelling <anna.moelling@googlemail.com>
2021-09-17 15:22:53 +0200

    testing signing in

--- spec/features/planets_spec.rb
+++ spec/features/planets_spec.rb
@@ -3,11 +3,33 @@ require 'rails_helper'
 RSpec.describe 'planets features' do
   describe 'viewing the index' do
     it 'lists all planets' do
-      u = User.create!(email: "anna@planet.de", password: 'foobar')
+      u = User.create!(email: "anna@web.de", password: 'foobar')
       Planet.create!(user: u, title: Faker::Movies::StarWars.planet, host_name: Faker::TvShows::Stargate.character)
       visit('/')
       click_link('Go Beyond')
       expect(page).to have_content('Galaxy Far, Far Away?')
     end
   end
+
+  describe 'adding a new planet' do
+    before do
+      user = User.create(email: 'test@test.com', password: "password", password_confirmation: "password") ## uncomment if not using FactoryBot
+      login_as(user)
+    end
+
+    it 'adds a planet to the list of planets' do
+      visit planets_path
+      click_link('List your planet')
+
+      expect(current_path).to have_content('/planets/new')
+
+      fill_in('Name', with: 'Test')
+      click_button('Publish')
+
+      expect(current_path).to have_content('/planets')
+      expect(page).to have_content('Test')
+
+    end
+  end
 end
+
---
+++ spec/features/users/sign_up_users_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "user signup" do
+  scenario "with valid credentials" do
+    visit "/" 
+    click_link "Go Beyond"
+    click_link "Login"
+    click_link "Sign up"
+    fill_in "Email", with: "vitor@web.br"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_button "Sign up"
+    expect(page).to have_content("You have signed up successfully.")
+    
+    visit "/"
+  end
+
+  scenario "with invalid credentials" do
+    visit "/"
+    click_link "Go Beyond"
+    click_link "Login"
+    click_link "Sign up"
+    fill_in "Email", with: ""
+    fill_in "Password",  with: ""
+    fill_in "Password confirmation",  with: ""
+    click_button "Sign up"
+
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+  end
+
+end
